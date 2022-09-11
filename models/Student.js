8871f4299c7e6fd582fc817f7960aa668557ba63
Alessandro de Jesus <deje0014@algonquinlive.com>
2022-09-10 20:53:08 -0400

    Created Student schema using mongoose

---
+++ models/Student.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose"
+
+const schema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            maxlength: 254,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            maxlength: 254,
+        },
+        email: {
+            type: String,
+            required: true,
+            maxlength: 254,
+        },
+        parent: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'Parent',
+            required: true,
+        }],
+        classes: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'Class',
+            required: true,
+        }],
+        professors: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'Professors',
+            required: true,
+        }]
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Model = mongoose.model('Student', schema)
+
+export default Model
