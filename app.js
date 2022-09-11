b05c50b35bedf2db93eac97499f9154d2102d3ce
Alessandro de Jesus <deje0014@algonquinlive.com>
2022-09-11 15:38:26 -0400

    Implemented handleErrors middleware

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-import express from 'express'
-import morgan from 'morgan'
-import sanitizeMongo from 'express-mongo-sanitize'
-import logger from './startup/logger.js'
-import authRouter from './routes/auth/index.js'
-import database from './startup/database.js'
+import express from "express";
+import morgan from "morgan";
+import sanitizeMongo from "express-mongo-sanitize";
+import logger from "./startup/logger.js";
+import authRouter from "./routes/auth/index.js";
+import database from "./startup/database.js";
+import handleErrors from "./middleware/handleErrors.js";
 
-const app = express()
-const log = logger.child({module: 'scholla:app'})
-database()
+const app = express();
+const log = logger.child({ module: "scholla:app" });
+database();
 
-log.info(process.env.NODE_ENV)
-log.warn(app.get('env')) //if NODE_ENV is undefined, returns development
+log.info(process.env.NODE_ENV);
+log.warn(app.get("env")); //if NODE_ENV is undefined, returns development
 
-app.use(morgan('tiny'))
-app.use(express.json())
-app.use(sanitizeMongo())
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(sanitizeMongo());
 
-app.get('/', (req, res) => {
-    res.send({data: {healthStatus: 'Running'}})
-})
-app.use('/auth', authRouter)
+app.get("/", (req, res) => {
+  res.send({ data: { healthStatus: "Running" } });
+});
+app.use("/auth", authRouter);
 
-export default app
+// error handler middleware
+app.use(handleErrors);
+
+export default app;
