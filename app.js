8571b3a45aa300e489c8356f1d5aa92d981929d8
Alessandro de Jesus <deje0014@algonquinlive.com>
2022-09-11 14:01:48 -0400

    Added protection against database injection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import sanitizeMongo from 'express-mongo-sanitize'
 import logger from './startup/logger.js'
 import authRouter from './routes/auth/index.js'
 import database from './startup/database.js'
@@ -13,6 +14,7 @@ log.warn(app.get('env')) //if NODE_ENV is undefined, returns development
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(sanitizeMongo())
 
 app.get('/', (req, res) => {
     res.send({data: {healthStatus: 'Running'}})
