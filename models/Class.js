e53e82c2870619a8514e756c550f3d9291bbcb5a
Alessandro de Jesus <deje0014@algonquinlive.com>
2022-09-10 21:20:29 -0400

    Created Class Schema

---
+++ models/Class.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+const schema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            maxlength: 254,
+        },
+        classCode: {
+            type: mongoose.Schema.Types.Mixed,
+            required: false
+        },
+        students: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'Student',
+        }],
+        professor: {
+            type: mongoose.Schema.Types.ObjectId, ref: 'Professor',
+        },
+        assignments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Assignment'}]
+    }
+)
+
+const Model = mongoose.model('Student', schema)
+
+export default Model
