b525575702f51fdc8fc5ddc8cbde0bddefd5bf0b
Alessandro <deje0014@algonquinlive.com>
2022-10-14 21:30:08 -0400

    Replaced database.js local variables by environment variables

--- startup/database.js
+++ startup/database.js
@@ -1,19 +1,21 @@
 import mongoose from "mongoose";
+import config from "config";
 import logger from "../startup/logger.js";
 
+const log = logger.child({module: 'connectDB'})
+
 export default async function database() {
+  const db = config.get("db");
+
   await mongoose
-    .connect("mongodb://localhost:27017/scholla-test", {
+    .connect(`mongodb://${db.host}:${db.port}/${db.dbName}`, {
       useNewUrlParser: true,
-    //   useCreateIndex: true,
-    //   useFindAndModify: false,
-    //   useUnifiedTopology: true,
     })
     .then(() => {
-      logger.info("Connected to MongoDB");
+      log.info("Connected to MongoDB");
     })
     .catch((err) => {
-      logger.error("Problem connecting to MongoDB.", err.message);
+      log.error("Problem connecting to MongoDB.", err.message);
       process.exit(1);
     });
 }
